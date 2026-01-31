404c7bd0033b84ba2bea7c82e853b0d42a32352a
alkislaya <41301020+AlKislaya@users.noreply.github.com>
2026-01-31 18:52:21 +0300

    Initial Commit

---
+++ Lunara/LunaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaraApp.swift
+//  Lunara
+//
+//  Created by Alexandra on 31.01.26.
+//
+
+import SwiftUI
+
+@main
+struct LunaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
