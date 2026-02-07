ab435ac36279cff15d49de85cf28b7ad603b5bd8
alkislaya <41301020+AlKislaya@users.noreply.github.com>
2026-02-07 12:11:19 +0300

    Add Opacity && Padding Styles

---
+++ Lunara/Core/Styles/Opacity.swift
@@ -0,0 +1,13 @@
+//
+//  AppCornerRadius.swift
+//  Lunara
+//
+//  Created by Alexandra on 2.02.26.
+//
+
+import Foundation
+
+enum Opacity {
+    static let container: CGFloat = 0.5
+    static let standartText: CGFloat = 0.6
+}
---
+++ Lunara/Core/Styles/Padding.swift
@@ -0,0 +1,14 @@
+//
+//  AppCornerRadius.swift
+//  Lunara
+//
+//  Created by Alexandra on 2.02.26.
+//
+
+import Foundation
+
+enum Padding {
+    static let standart: CGFloat = 8
+    static let medium: CGFloat = 16
+    static let large: CGFloat = 24
+}
